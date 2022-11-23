61645023354e9672168bf5b6e068e71ab2ca43e2
pauline-njunguna <paulinenjunguna.w@gmail.com>
2022-11-23 10:40:53 +0300

    Done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let num1 = 31;
 let num2 = 2;
 const multiply= num1* num2;
 
+
 let random= Math.floor(Math.random() * 10) + 1;
 
 let num3= 58;
@@ -12,3 +13,5 @@ const mod= num3%num4;
 
 
 const max = Math.max(5, 10 ,15 ,20)
+
+6*4
